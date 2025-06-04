b5b6caab270f063107a34f7ad6464ae9d486e92a
Tosin Ekundayo <tosinekshally@gmail.com>
2025-06-04 14:48:02 +0100

    worked on the user

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 // apiConfig.js
-const API_BASE_URL = 'http://localhost:1970/api';
-// const API_BASE_URL = 'https://aeroplane-backend.onrender.com/api';
+// const API_BASE_URL = 'http://localhost:1970/api';
+const API_BASE_URL = 'https://aeroplane-backend.onrender.com/api';
 
 
 module.exports= {API_BASE_URL, 
